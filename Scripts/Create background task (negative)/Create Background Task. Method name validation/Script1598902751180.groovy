ddad2b7ca45411a1e89629ed50d74abb76b22dae
Nastassia Babkova <aubobkova@gmail.com>
2020-08-31 22:39:48 +0300

    Added negative tests to create task with validation

---
+++ Scripts/Create background task (negative)/Create Background Task. Method name validation/Script1598902751180.groovy
@@ -0,0 +1,44 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+// arrange
+def id = UUID.randomUUID().toString()
+
+def name = 'new background task'
+
+def description = 'first backgroundTask'
+
+def declaringTypeFullName = 'Suvoda.SuperNamespace.SuperClass'
+
+def serviceName = 'SubjectService'
+
+def statusCode = expectedStatusCode as int
+
+// act
+def response = WS.sendRequest(findTestObject('Background Tasks/Create/Create Background Task', [('URL') : GlobalVariable.URL, ('id') : id
+            , ('name') : name, ('description') : description, ('methodName') : methodName, ('declaringTypeFullName') : declaringTypeFullName
+            , ('serviceName') : serviceName]))
+
+// assert
+WS.verifyResponseStatusCode(response, statusCode)
+
+// clean
+if(statusCode == 200)
+{
+WS.sendRequestAndVerify(findTestObject('Background Tasks/Delete/Delete Background Task By Id', [('URL') : GlobalVariable.URL
+            , ('id') : id]))
+}
