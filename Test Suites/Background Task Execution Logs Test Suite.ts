1cf80c0c2fd92ebb22a023ee04c1d09fdc9f7047
Nastassia Babkova <aubobkova@gmail.com>
2020-09-02 21:18:55 +0300

    Added Test Suite for task logs

---
+++ Test Suites/Background Task Execution Logs Test Suite.ts
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Background Task Execution Logs Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6a30a1be-4008-49b5-82f4-8c15278e64fa</testSuiteGuid>
+   <testCaseLink>
+      <guid>f3e7542f-55b9-4870-9dbf-c13b687f8cb8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log (positive flow)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7b7c715b-49eb-4432-98cb-89e7919b0fb5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log with empty body</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1fac03f3-b122-4305-9f5c-981528e5d34c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log with empty key message</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b74e7d31-2fda-4f14-8fb6-4098919dd034</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log with non-existing executionId</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4b52c279-6ed3-49b4-9949-e4cb9cb61535</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log with not string message</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9439e1a-6f6e-4806-b53b-00a0a8ec5a45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log with not unique id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>340c5e81-28e7-4f42-8093-c68dbb0b820e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log without executionId</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0a7a4ad1-6520-457a-8148-fac7dab99cd8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log without message</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a640f596-7ad0-4442-8105-abfe60d61f5b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log without optional key id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db9761bd-f42f-4b40-ad13-e3a88ed97fa0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log. ExecutionId validation</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>7bdd718f-41f0-48e3-b604-6cb21c14e0a0</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>348c0f6d-d767-4824-96c2-aa1c110728ba</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2e5b63e-8586-4317-ae3b-5ba15f6a6d5e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Log. Id validation</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c0c0ef76-81db-4e91-90c4-ed8a849a904f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2e284c73-9dee-4783-b4b3-864b1ce7ac95</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dd0ac1fa-668e-49c0-a401-67900401c180</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Create Background Task Executions Logs/Create Background Task Execution Logs with the same message</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b24bd040-b8d1-4f90-9fd1-53d48974ecb5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Get Background Task Executions Logs/Get Background Task Execution Logs by Execution Id with no logs</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>09fc7331-dabb-4149-8a35-035d8287e66c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Get Background Task Executions Logs/Get Background Task Execution Logs by Execution Id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e20eeb5-4398-4538-ad89-9a7af945889e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Get Background Task Executions Logs/Get Background Task Execution Logs by invalid Execution Id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43a35896-63e0-4ef9-a3f3-24421e2650a2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Get Background Task Executions Logs/Get Background Task Execution Logs by non-existing Execution Id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ea8cccca-a0a2-40e5-bb14-287f18ac6630</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Background Task Executions Logs/Get Background Task Executions Logs/Get Background Task Execution Logs combine pagination, filtering, sorting</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
