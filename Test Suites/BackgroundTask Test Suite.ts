5ba2b434285b3e091e20cf39cda025ba1e3644ba
Nastassia Babkova <aubobkova@gmail.com>
2020-09-02 21:17:31 +0300

    Added Test Suite for Background Task

---
+++ Test Suites/BackgroundTask Test Suite.ts
@@ -0,0 +1,295 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BackgroundTask Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>33af9a9f-f972-4901-9e2d-c77556d118db</testSuiteGuid>
+   <testCaseLink>
+      <guid>06a03a5b-10cc-40ec-9a4e-2fdbbde467e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (DeclaringTypeFullName is invalid)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3183bcd9-9799-4e65-a99f-c27fb822896a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (DeclaringTypeFullName is not string)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>00386704-5453-477a-b2c4-782d2513b217</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (DeclaringTypeFullName without parameter)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ab3eb70d-1eb5-4a58-aba7-c781023aae88</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (description is not a string)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33db9207-244e-4e9e-8169-f09e72a51cbe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (methodName is invalid method name)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2c1d04dd-97f5-4d4f-8770-9ba2dafe69b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (methodName is not a string)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c7faec83-61f6-4ef3-b5c8-529c0e0e910d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (methodName without parameter)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3eba083-f770-4560-998f-59cc53f115a9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (name is not a string)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>72692833-44c1-48ab-85d8-2df984205046</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task (negative flow)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>912dcbcf-564b-4547-9adb-d2f98e78aaad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task with id (null)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d9c85d8f-acb4-4c3f-bee6-475541986eff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task with invalid id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c0dc27f7-dd26-4a9f-8efd-c7eddee5009d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create background task with not string id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>13655ce2-54ac-4da6-84b8-c4ad57363e88</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create Background Task. DeclaringTypeFullName validation</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>5d01a822-febe-4bdf-95d9-1559e66f5962</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>2f80cfa9-8474-4df4-924a-c8b867d81204</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e9d69ed-6c98-4287-a08c-29bf3b9cd9d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (negative)/Create Background Task. Method name validation</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>34a0f73c-8b6d-41b1-b780-63f8be51edfe</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d4ef8b36-f87a-482a-8569-84294832cde0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>95455ef2-be61-4a1b-9e25-518f5df50584</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create backgroud task without optional key (description)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>15b2a502-3059-405f-ad55-7e70378a1eb4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create background task (positive flow)</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d7e54313-e6fd-4b15-be35-3dd1189c8bea</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0f18b66e-e7fc-45c1-8a1e-26bf0f8d9fe0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cecc8a02-e2e0-43ee-ac24-b47eebce1510</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create background task with empty description</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e8625208-12d4-4433-9bc6-2444adf13c0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create background task with empty name</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c427137f-c830-4450-9966-47ebdd3e2741</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create background task with random order of keys</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d89eb0ec-a8ec-4544-8e46-342e157e8e3b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create background task without all optional keys</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f52bf229-6732-48fa-b592-7d1d181cd9c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create background task without optional key (id)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7230db4b-2ea9-4348-a4ba-c4d968e3421c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create background task without optional key (name)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6630c96f-81ba-4ef0-b7a1-7bbd1f6fcc5b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create background task (positive)/Create background task without optional keys (default values in ArgumentDescription)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4fd1ccbe-4271-4d1e-8b15-301b3ee7c975</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Delete background task (negative)/Delete background task by invalid task Id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0152f93b-b0a0-4fbb-a67e-975454e35bbc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Delete background task (negative)/Delete background task by non-existing task Id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>37217692-2a07-4861-b3da-492347fd7697</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Delete background task (positive)/Delete background task ALL</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>abee8d8b-34cd-4a33-990a-9c2ba8e99321</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Delete background task (positive)/Delete background task by existing task Id</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0d3bafc-ca2c-4b2b-8b9f-9da600319728</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Delete background task (positive)/Delete background task when there are no registered tasks</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e9a503e6-5349-4e21-abda-16f3fd51d26c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Filter and Sort Background Task by ServiceName</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>11b7a1e8-5fa0-484f-9c90-50d87b797793</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Filter Background Task by Description</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>67365f0e-b5fe-4dc0-acdc-2df55497ffb6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Filter Background Task by MethodName</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e03c2ad5-9c73-4d2e-9f03-b3cb721c7c41</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Filter Background Task by Name</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>721ffa1b-2379-4521-83e0-f3f2f6059ebf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Filter BackgroundTask by DeclaringTypeFullName</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3471b5b6-5c9b-4545-8616-af788c09d064</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Get Background Task by ID positive</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>957f4fe9-cd7a-44d2-a5d1-21474f201e46</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Get Background Task by Invalid ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf6073ba-24f8-4b02-8c15-b153986bcc2a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Get Background Task by Not Existing ID</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4c285c7c-b9db-45cb-8869-87e385e93897</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Get Background Tasks/Pagination Element Count</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e3558793-0fd2-4aaa-81cd-e87bd4b6478c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>985ab1a9-e7f8-4ad6-842a-2c31793cd2c2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>9aba8dc8-016b-4ff0-8e38-1ca3ef3257ce</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>ae4c23a4-3883-4006-9e12-ead3538476ba</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
